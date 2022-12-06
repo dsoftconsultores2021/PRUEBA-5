d5938e42c0e58d7fcaaba5374145949242e11875
dsoftconsultores2021 <93953483+dsoftconsultores2021@users.noreply.github.com>
2022-12-06 13:54:26 -0500

    Modulo IoTapp desarrollo CONECTSEN
    Modulo de prueba

---
+++ iotapp/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name":"IoT App",
+    "description":"Aplicacion para reportear datos IoT",
+    "author":"antonio.silvera@conectsen.com",
+    "version":"1.0",
+    "depends":["base"],
+    "data":[
+        "security/ir_rule.xml",
+        "security/res_groups.xml",
+        "security/security.xml",
+        "views/views.xml"
+    ]
+}
